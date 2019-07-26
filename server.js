654e5c290a79a33ac9567d6a90dd4a6cb248a8c6
Chris Milson <chris.w.milson@gmail.com>
2019-07-26 11:22:57 +0900

    Check server port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.get('/api/customers', (req, res) => {
   const customers = [
