9ab3b0323dbde42fdc1d4c1151ebd128a2b46965
Chris Milson <chris.w.milson@gmail.com>
2019-07-26 11:09:01 +0900

    Serve client

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.port || 5000;
 
 app.get('/api/customers', (req, res) => {
   const customers = [
-    {id: 1, firstName: 'John', lastName: 'Doe'},
-    {id: 2, firstName: 'Brad', lastName: 'Traversy'},
-    {id: 3, firstName: 'Mary', lastName: 'Swanson'},
+    { id: 1, firstName: 'John', lastName: 'Doe' },
+    { id: 2, firstName: 'Brad', lastName: 'Traversy' },
+    { id: 3, firstName: 'Mary', lastName: 'Swanson' },
   ];
 
   res.json(customers);
 });
+
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`))
