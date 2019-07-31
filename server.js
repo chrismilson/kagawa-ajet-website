77edebb05253b46c5271d0913e03a0570528951e
Chris Milson <chris.w.milson@gmail.com>
2019-07-31 16:58:37 +0900

    Clean server.js

--- server.js
+++ server.js
@@ -1,22 +1,13 @@
-const express = require('express');
-const path = require('path');
+const express = require('express')
+const path = require('path')
 
-const app = express();
-const PORT = process.env.PORT || 5000;
+const app = express()
+const PORT = process.env.PORT || 5000
 
-app.get('/api/customers', (req, res) => {
-  const customers = [
-    { id: 1, firstName: 'John', lastName: 'Doe' },
-    { id: 2, firstName: 'Brad', lastName: 'Traversy' },
-    { id: 3, firstName: 'Mary', lastName: 'Swanson' },
-  ];
+app.use(express.static(path.join(__dirname, 'client/build')))
 
-  res.json(customers);
-});
-
-app.use(express.static(path.join(__dirname, 'client/build')));
 app.get('*', (req, res) => {
-  res.sendfile(path.join(__dirname = 'client/build/index.html'));
-});
+  res.sendfile(path.join(__dirname, 'client/build/index.html'))
+})
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`))
