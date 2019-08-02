1273381dbe071b64eeda04eddbc00d4822c88c0a
Chris Milson <chris.w.milson@gmail.com>
2019-08-02 18:07:57 +0900

    Create calendar get route

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const express = require('express')
 const path = require('path')
+const axios = require('axios')
 
 const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(express.static(path.join(__dirname, 'client/build')))
 
+app.get('/api/calendar', (req, res) => {
+  axios.get('https://calendar.google.com/calendar/ical/gprr6e1so5bm32gjig3vf5ehk8%40group.calendar.google.com/public/basic.ics')
+    .then(calRes => {
+      res.json(calRes.data)
+    })
+    .catch(err => console.log(err))
+})
+
 app.get('*', (req, res) => {
-  res.sendfile(path.join(__dirname, 'client/build/index.html'))
+  res.sendFile(path.join(__dirname, 'client/build/index.html'))
 })
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`))
