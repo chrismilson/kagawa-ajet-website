bb4fd7063ea74b3f1fc962f51c681fd32a7727a2
Chris Milson <chris.w.milson@gmail.com>
2019-08-06 12:51:15 +0900

    Add contact information

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import Page from './Page'
+import { FaLine } from 'react-icons/fa'
 
 function Contact (props) {
   return (
     <Page className='Contact'>
       <div className='col text'>
-        <h1>Get in touch</h1>
-        <p>
-          We are available where you need us!
-        </p>
+        <a href='http://nav.cx/gVHcg1H'>
+          <div className='social line'>
+            <h1><FaLine /> Line</h1>
+            <img
+              src='https://qr-official.line.me/sid/M/876fxbyc.png'
+              alt='Line QR Code'
+            />
+          </div>
+        </a>
       </div>
     </Page>
   )
