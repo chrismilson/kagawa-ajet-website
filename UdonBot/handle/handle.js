861f7ad8f38d0dd53ec4eee03dd5d7c4467dbb3d
Chris Milson <chris.w.milson@gmail.com>
2019-08-07 22:29:04 +0900

    Change bot handling

--- UdonBot/handle/handle.js
+++ UdonBot/handle/handle.js
@@ -1,4 +1,8 @@
 function handle (event) {
+  if (event.replyToken && event.replyToken.match(/^(.)\1*$/)) {
+    return console.log('Test hook recieved: ' + JSON.stringify(event.message))
+  }
+
   return {
     type: 'reply',
     reply: {
--- UdonBot/respond/template/template.js
+++ UdonBot/respond/template/template.js
@@ -51,7 +51,7 @@ function quietAnnounce (client, token, data) {
         })))
       }
     }
-  }, true)
+  })
 }
 
 module.exports = {
