f388978385e9502a0afe8f7f81a64d283763e21a
Chris Milson <chris.w.milson@gmail.com>
2019-08-08 16:04:32 +0900

    Render descriptions with markdown

--- client/src/pages/Resources.js
+++ client/src/pages/Resources.js
@@ -2,55 +2,35 @@ import React from 'react'
 import Page from './Page'
 import { Route, Link } from 'react-router-dom'
 import { FaExternalLinkAlt, FaEnvelope } from 'react-icons/fa'
+import ReactMarkdown from 'react-markdown'
 
 import './Resources.scss'
 import NewJetResources from './NewJetResources'
+import renderers from './renderers'
 
 var res = [
   {
     name: 'New JET Resources',
     type: 'local',
     path: '/resources/new-jets/',
-    description: `
-      Some resources we put together for incoming JETs.
-    `
+    description: 'Some resources we put together for incoming JETs.'
   },
   {
     name: 'National AJET',
     type: 'link',
     link: 'https://ajet.net/',
-    description: `
-      The National AJET page is a great resource for all JETs.
-    `
+    description: 'The National AJET page is a great resource for all JETs.'
   },
   {
     name: 'Tofugu',
     type: 'link',
     link: 'https://www.tofugu.com/',
-    description: [
-      `
-      Tofugu is a great place to start if you want to learn about
-      anything Japan.
-      They also have
-      `,
-      <a
-        href='https://www.tofugu.com/japan/jet-program-teaching-resources/'
-        target='_blank'
-        rel='noopener noreferrer'
-        key='open'
-      >
-        resources for JETs
-      </a>,
-      '.'
-    ]
+    description: 'Tofugu is a great place to start if you want to learn about anything Japan. They also have [resources for JETs](https://www.tofugu.com/japan/jet-program-teaching-resources/).'
   },
   {
     name: 'Marugame Japanese Lessons',
     type: 'email',
-    description: `
-      Nishi Sensei does a lesson every week on Wednesday Night in Marugame.
-      Get in touch with her by email!
-    `,
+    description: 'Nishi Sensei does a lesson every week on Wednesday Night in Marugame. Get in touch with her by email!',
     email: {
       address: 'mkokusaikoryu-561771@mountain.ocn.ne.jp',
       subject: 'Japanese Lessons in Marugame'
@@ -60,35 +40,25 @@ var res = [
     name: 'IPAL Kagawa',
     type: 'link',
     link: 'http://www.i-pal.or.jp/en/whats-i-pal.html',
-    description: `
-      IPAL is a place in Takamatsu that has cultural events for foreigners
-      and Japanese alike. They have Japanese lessons, international cooking
-      lessons and much more!
-    `
+    description: 'IPAL is a place in Takamatsu that has cultural events for foreigners and Japanese alike. They have Japanese lessons, international cooking lessons and much more!'
   },
   {
     name: 'Teaching Principals by MEXT',
     type: 'link',
     link: 'http://www.britishcouncil.jp/sites/britishcouncil.jp/files/alt-handbook-en_0.pdf',
-    description: `
-      This book is a great read for experienced teachers. It's a little long but holds about the same information as a TEFL course.
-    `
+    description: 'This book is a great read for experienced teachers. It\'s a little long but holds about the same information as a TEFL course.'
   },
   {
     name: 'ALTopedia',
     type: 'link',
     link: 'https://www.altopedia.org/',
-    description: `
-      This is a great resource for JHS teaching. It explains all the textbooks and breaks down all the lessons into a form that is easy to understand.
-    `
+    description: 'This is a great resource for JHS teaching. It explains all the textbooks and breaks down all the lessons into a form that is easy to understand.'
   },
   {
     name: 'Education.com',
     type: 'link',
     link: 'https://www.education.com/worksheets/ela/',
-    description: `
-      So many worksheets you'll never need another one.
-    `
+    description: 'So many worksheets you\'ll never need another one.'
   }
 ]
 
@@ -145,9 +115,11 @@ function ResourceMemo (props) {
   return (
     <div className='ResourceMemo'>
       {title}
-      <div className='description'>
-        {props.res.description}
-      </div>
+      <ReactMarkdown
+        className='description'
+        source={props.res.description}
+        renderers={renderers}
+      />
     </div>
   )
 }
