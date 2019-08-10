b81d285583edc05438a6a0e73ffc7fe8f2699d73
Chris Milson <chris.w.milson@gmail.com>
2019-08-10 22:40:44 +0900

    Add email address

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -6,15 +6,25 @@ function Contact (props) {
   return (
     <Page className='Contact'>
       <div className='col text'>
-        <a href='http://nav.cx/gVHcg1H'>
-          <div className='social line'>
-            <h1><FaLine /> Line</h1>
+        <div className='email'>
+          <h1>Email</h1>
+          <a
+            href='mailto:udonkenajet@gmail.com'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            udonkenajet@gmail.com
+          </a>
+        </div>
+        <div className='social line'>
+          <h1><FaLine /> Line</h1>
+          <a href='http://nav.cx/gVHcg1H'>
             <img
               src='https://qr-official.line.me/sid/M/876fxbyc.png'
               alt='Line QR Code'
             />
-          </div>
-        </a>
+          </a>
+        </div>
       </div>
     </Page>
   )
