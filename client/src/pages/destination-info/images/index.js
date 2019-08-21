984ed4be2c3cd544f1ca390ac6f803db7030da18
Chris Milson <chris.w.milson@gmail.com>
2019-08-21 15:53:45 +0900

    Add Naoshima page

--- client/src/pages/destination-info/images/index.js
+++ client/src/pages/destination-info/images/index.js
@@ -1,9 +1,11 @@
 import * as kanonji from './kanonji'
 import * as mitoyo from './mitoyo'
 import * as marugame from './marugame'
+import * as naoshima from './naoshima'
 
 export {
   kanonji,
   mitoyo,
-  marugame
+  marugame,
+  naoshima
 }
---
+++ client/src/pages/destination-info/images/naoshima/index.js
@@ -0,0 +1,3 @@
+export { default as libertyOfStatue } from './liberty-of-statue.jpg'
+export { default as yellowPumpkin } from './yellow-pumpkin.jpg'
+export { default as chichu } from './chichu.jpg'
