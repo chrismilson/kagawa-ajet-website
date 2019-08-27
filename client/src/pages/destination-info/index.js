709b32ce3445c8dd15642fa3cb98176b41ccf5c4
Chris Milson <chris.w.milson@gmail.com>
2019-08-27 09:38:20 +0900

    Use kanji names as well

--- client/src/pages/destination-info/index.js
+++ client/src/pages/destination-info/index.js
@@ -4,23 +4,27 @@ import * as images from './images'
 export { images }
 
 export default [
-  'Ayagawa',
-  'Kanonji',
-  'Kotohira',
-  'Mannō',
-  'Marugame',
-  'Mitoyo',
-  'Naoshima',
-  // 'Sakaide',
-  // 'Sanuki',
-  // 'Shodoshima',
-  // 'Takamatsu',
-  'Zentsuji'
-].map(p => ({
-  name: p,
-  path: p.toLowerCase().replace(/ /g, '-'),
-  md: require('./pages/' + p.toLowerCase().replace(/ /g, '-') + '.md'),
-  thumbnail: thumbs[p.toLowerCase().split(' ').map((word, i) => {
-    return i === 0 ? word : word.charAt(0).toUpperCase() + word.substr(1)
-  }).join('')]
-}))
+  'Ayagawa - 綾川',
+  'Kanonji - 観音寺',
+  'Kotohira - 琴平',
+  'Mannō - 満農',
+  'Marugame - 丸亀',
+  'Mitoyo - 三豊',
+  'Naoshima - 直島',
+  // 'Sakaide - 坂出',
+  // 'Sanuki - 讃岐',
+  // 'Shodoshima - 小豆島',
+  // 'Takamatsu - 高松',
+  'Zentsuji - 善通寺'
+].map(name => {
+  var place = name.split(/ - /)[0]
+
+  return {
+    name: name,
+    path: place.toLowerCase().replace(/ /g, '-'),
+    md: require('./pages/' + place.toLowerCase().replace(/ /g, '-') + '.md'),
+    thumbnail: thumbs[place.toLowerCase().split(' ').map((word, i) => {
+      return i === 0 ? word : word.charAt(0).toUpperCase() + word.substr(1)
+    }).join('')]
+  }
+})
