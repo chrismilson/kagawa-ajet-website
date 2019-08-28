8749d74b43c5d23c39fe754306566f74b8d4a703
Chris Milson <chris.w.milson@gmail.com>
2019-08-28 10:22:14 +0900

    Improve status messages

--- subscribers.js
+++ subscribers.js
@@ -21,7 +21,7 @@ var db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error:'))
 
 db.once('open', () => {
-  console.log('connected to database!')
+  console.log('Connected to subscriber database')
 })
 
 const forAll = function (action) {
